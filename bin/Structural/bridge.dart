a0e5f4c6b015fb810c611dbeb42ec7af6217a290
mostafa hemmati <mostafa.hemmati1362@gmail.com>
2021-11-19 21:07:44 +0330

    bridge pattern

---
+++ bin/Structural/bridge.dart
@@ -0,0 +1,44 @@
+void main(List<String> args) {
+  Shape redCircle = Circle(100, 100, 10, RedCircle());
+  Shape greenCircle = Circle(100, 100, 10, GreenCircle());
+
+  redCircle.draw();
+  greenCircle.draw();
+}
+
+class DrawAPI {
+  void drawCircle(int radius, int x, int y) {}
+}
+
+class RedCircle implements DrawAPI {
+  @override
+  void drawCircle(int radius, int x, int y) {
+    print("Drawing Circle[ color: red, radius: $radius, x:  $x , y: $y ]");
+  }
+}
+
+class GreenCircle implements DrawAPI {
+  @override
+  void drawCircle(int radius, int x, int y) {
+    print("Drawing Circle[ color: green, radius:  $radius, x:  $x , y: $y ]");
+  }
+}
+
+abstract class Shape {
+  late DrawAPI drawAPI;
+
+  Shape(this.drawAPI);
+
+  void draw();
+}
+
+class Circle extends Shape {
+  int x, y, radius;
+
+  Circle(this.x, this.y, this.radius, drawAPI) : super(drawAPI);
+
+  @override
+  void draw() {
+    drawAPI.drawCircle(radius, x, y);
+  }
+}
