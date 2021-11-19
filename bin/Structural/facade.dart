089a6bf87e5910ba2ae150b5c50f5dd95aa31481
mostafa hemmati <mostafa.hemmati1362@gmail.com>
2021-11-20 00:13:13 +0330

    facade pattern

---
+++ bin/Structural/facade.dart
@@ -0,0 +1,40 @@
+void main(List<String> args) {
+  ShapeMaker shapeMaker = ShapeMaker();
+
+  shapeMaker.drawCircle();
+  shapeMaker.drawRectangle();
+}
+
+class Shape {
+  void draw() {}
+}
+
+class Rectangle implements Shape {
+  @override
+  void draw() {
+    print("Rectangle::draw()");
+  }
+}
+
+class Circle implements Shape {
+  @override
+  void draw() {
+    print("Circle::draw()");
+  }
+}
+
+class ShapeMaker {
+  late Shape circle;
+  late Shape rectangle;
+  ShapeMaker() {
+    circle = Circle();
+    rectangle = Rectangle();
+  }
+  void drawCircle() {
+    circle.draw();
+  }
+
+  void drawRectangle() {
+    rectangle.draw();
+  }
+}
