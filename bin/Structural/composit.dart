8f6f105f365097fad2b64f5611ba20722306e8e3
mostafa hemmati <mostafa.hemmati1362@gmail.com>
2021-11-19 23:07:23 +0330

    composite pattern

---
+++ bin/Structural/composit.dart
@@ -0,0 +1,66 @@
+void main(List<String> args) {
+  Company engDirectory = Company("engDirectory");
+  Developer dev1 = Developer(100, "Mostafa hemati", "Pro Developer");
+  Developer dev2 = Developer(101, "hassan  kamrani", "Developer");
+  engDirectory.addEmployee(dev1);
+  engDirectory.addEmployee(dev2);
+
+  Manager man1 = Manager(200, "mahdi zareei", "Sr Manager");
+  Manager man2 = Manager(201, "ramin Jafari", "Manager");
+  Company accDirectory = Company("accDirectory");
+  accDirectory.addEmployee(man1);
+  accDirectory.addEmployee(man2);
+
+  Company directory = Company("directory");
+  directory.addEmployee(engDirectory);
+  directory.addEmployee(accDirectory);
+  directory.showEmployeeDetails();
+}
+
+class Employee {
+  void showEmployeeDetails() {}
+}
+
+class Developer implements Employee {
+  late final String _name;
+  late final int _emId;
+  late final String _position;
+  Developer(this._emId, this._name, this._position);
+  @override
+  void showEmployeeDetails() {
+    print("name : $_name  Id : $_emId  position : $_position");
+  }
+}
+
+class Manager implements Employee {
+  late final String _name;
+  late final int _emId;
+  late final String _position;
+  Manager(this._emId, this._name, this._position);
+  @override
+  void showEmployeeDetails() {
+    print("name : $_name  Id : $_emId  position : $_position");
+  }
+}
+
+class Company implements Employee {
+  final List<Employee> _employeeList = <Employee>[];
+  late final String _name;
+  Company(this._name);
+
+  @override
+  void showEmployeeDetails() {
+    print("Company  $_name  detail :");
+    for (Employee e in _employeeList) {
+      e.showEmployeeDetails();
+    }
+  }
+
+  void addEmployee(Employee e) {
+    _employeeList.add(e);
+  }
+
+  void removeEmployee(Employee e) {
+    _employeeList.remove(e);
+  }
+}
